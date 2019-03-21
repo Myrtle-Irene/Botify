7eac69bda1c9403eb3ec1da7976427bd51bc7cba
Myrtle-Irene <Myrtle15Irene@gmail.com>
2019-03-21 16:33:35 +0300

    Update db_query.py

--- db_query.py
+++ db_query.py
@@ -1,8 +1,8 @@
 import os
 
-def get_sql(db=r'E:\Торренты\Coding\11\1111\ln2sql\ln2sql\database_store\city.sql',
-	vocab=r'E:\Торренты\Coding\11\1111\ln2sql\ln2sql\lang_store/english.csv',
-	query='Count how many city there are with the name blob?'):
+def get_sql(db=r'E:\Торренты\Coding\11\botify\ln2sql\ln2sql\database_store\city.sql',
+    vocab=r'E:\Торренты\Coding\11\botify\ln2sql\ln2sql\lang_store\english.csv',
+    query='Count how many city there are with the name blob?'):
 
-	os.system('python -m ln2sql.main -d {} -l {} -j output.json -i \
-		"{}"'.format(db, vocab, query))
+    os.system('python -m ln2sql.main -d {} -l {} -j output.json -i \
+    	"{}"'.format(db, vocab, query))
