01e0843a773e9b9b1bdee19141d3ab94d277f94c
Myrtle-Irene <myrtle15irene@gmail.com>
2019-03-22 08:09:13 +0300

    1

--- db_query.py
+++ db_query.py
@@ -1,9 +1,6 @@
 import os
-
-def get_sql(db=r'database_store/city.sql',
-    vocab=r'lang_store/english.csv',
-    query='Count how many city there are with the name blob?'):
-
-    os.system(r'python -m ln2sql.main -d {} -l {} -j output.json -i "{}"'.format(db, vocab, query))
+def get_sql(db=r'ln2sql/database/city.sql', vocab=r'ln2sql/lang/english.csv', 
+	query=r'how many city there are in which the employee name is similar to aman ?'):
+	os.system(r'''C:\Python27\python.exe ln2sql/ln2sql.py -d {} -l {} -j output.json -i "{}"'''.format(db,vocab, query))
 
 get_sql()
