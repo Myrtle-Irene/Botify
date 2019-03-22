6e390107b81e22971921f5e7ba47e479a40a227c
Myrtle-Irene <myrtle15irene@gmail.com>
2019-03-22 09:44:53 +0300

    1

--- db_query.py
+++ db_query.py
@@ -5,6 +5,6 @@ def get_sql(db=r'ln2sql/database/city.sql', vocab=r'ln2sql/lang/english.csv',
 
 
 
-heroes3 = (r'heroes3/units.sql', r'ln2sql/lang/english.csv', r'Att of units with Name  Dwarf')
+heroes3 = (r'heroes3/units.sql', r'ln2sql/lang/english.csv', r'how many units with Name  Dwarf')
 get_sql(*heroes3)
 #get_sql()
