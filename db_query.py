be96d4c50d61fd310558e0b99f417e8631fd388f
Myrtle-Irene <myrtle15irene@gmail.com>
2019-03-22 09:11:05 +0300

    1

--- db_query.py
+++ db_query.py
@@ -3,4 +3,8 @@ def get_sql(db=r'ln2sql/database/city.sql', vocab=r'ln2sql/lang/english.csv',
 	query=r'how many city there are in which the employee name is similar to aman ?'):
 	os.system(r'''C:\Python27\python.exe ln2sql/ln2sql.py -d {} -l {} -j output.json -i "{}"'''.format(db, vocab, query))
 
+
+
+heroes3 = (r'heroes3/units.sql', r'ln2sql/lang/english.csv', r'Att less than 15')
+#get_sql(*heroes3)
 get_sql()
